5cb17a8ee135df28e05565aadcb16243ba9d6b46
Benoit CIVEL <bcivel@users.noreply.github.com>
2022-06-12 22:39:31 +0200

    Delete newfile.js

--- newfile.js
+++
@@ -1,5 +0,0 @@
-let names = ["Mike","Matt","Nancy","Adam","Jenny","Nancy","Carl"];
-let uniqueNames = [];
-$.each(names, function(_i, el){
-    if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
-});
