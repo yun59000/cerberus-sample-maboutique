14f603c4388cdfe69508ab9381b6281d34ea1ac4
Benoit CIVEL <bcivel@users.noreply.github.com>
2022-06-12 22:13:20 +0200

    Update newfile.js

--- newfile.js
+++ newfile.js
@@ -1,5 +1,5 @@
-var names = ["Mike","Matt","Nancy","Adam","Jenny","Nancy","Carl"];
-var uniqueNames = [];
-$.each(names, function(i, el){
+let names = ["Mike","Matt","Nancy","Adam","Jenny","Nancy","Carl"];
+let uniqueNames = [];
+$.each(names, function(_i, el){
     if($.inArray(el, uniqueNames) === -1) uniqueNames.push(el);
 });
